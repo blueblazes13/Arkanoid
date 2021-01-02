a4c0b11e654230eddeb1d4c547a447a59e183f31
blueblazes13 <blueblazes13@gmail.com>
2021-01-02 23:54:30 +0100

    Revert "Begin gemaakt + uitleg over block object"
    This reverts commit b0b9777599fd50a00fd08ebaf057f7f35e618bec.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.lawajo.arkanoid {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.base;
 
     opens com.lawajo.arkanoid to javafx.fxml;
     exports com.lawajo.arkanoid;
