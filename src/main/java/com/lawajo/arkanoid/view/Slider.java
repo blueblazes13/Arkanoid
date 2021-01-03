645d7b71e8c6f67a52ba5c583dadae7c1f84975b
blueblazes13 <blueblazes13@gmail.com>
2021-01-03 12:17:40 +0100

    Eerste functies + javadoc gemaakt

--- src/main/java/com/lawajo/arkanoid/view/Slider.java
+++ src/main/java/com/lawajo/arkanoid/view/Slider.java
@@ -9,6 +9,48 @@ package com.lawajo.arkanoid.view;
  *
  * @author joeyk
  */
-public class Slider {
+public class Slider extends Block {
+    
+    private int speed;
+    
+    
+    
+    /**
+     * Initializes a new slider.
+     * 
+     * @param x The x coordinate of the slider.
+     * @param y The y coordinate of the slider.
+     */
+    public Slider(int x, int y) {
+        super(x, y);
+        
+        this.speed = 1;
+    }
+    
+    
+    /**
+     * Moves the slider to the left.
+     */
+    public void moveLeft() {
+        super.setX(super.getX() - speed);
+    }
+    
+    
+    /**
+     * Moves the slider to the right.
+     */
+    public void moveRight() {
+        super.setX(super.getX() + speed);
+    }
+    
+    
+    /**
+     * Sets the speed of the slider.
+     * 
+     * @param speed The speed of the slider.
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
     
 }
